05866e662c2acea645ce39afacb06f79d6668e5b
Paul Harden <paulharden@Pauls-MacBook-Pro.local>
2019-02-11 17:50:36 -0500

    Solution to the Madlibs exercise

---
+++ madlib.py
@@ -0,0 +1,10 @@
+def main():
+    noun = input("Enter a noun:")
+    verb = input("Enter a verb:")
+    adj = input("Enter an adjective:")
+    place = input("Enter a place:")
+
+    print("The",adj,noun,"is constantly trying to",verb,"whenever it arrives in",place)
+    
+
+main()
