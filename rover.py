4957960f951237c9639cdb44780d8b049b9dc76b
Paul Harden <paulharden@Pauls-MacBook-Pro.local>
2019-02-11 17:27:45 -0500

    Solution to Curiosity rover exercise

---
+++ rover.py
@@ -0,0 +1,12 @@
+# Calculate the time it takes an image sent from Curiosity to arrive at NASA
+
+def main():
+    speed = 186000
+    dist = 34000000
+    answer = (34000000 / 186000)
+
+    print("It would only take", answer, "seconds for an image to arrive from Curiosity")
+
+main()
+
+
