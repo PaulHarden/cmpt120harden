ac5262e9ce295efba1c619e8acd1d8df0544acbf
Juan Arias <juan.arias@marist.edu>
2019-02-24 10:44:17 -0500

    Feedback

--- fibonacci.py
+++ fibonacci.py
@@ -4,8 +4,9 @@ import math
 
 def main():
     n = int(input("What step of the Fibonacci sequence would you like: "))
-    old,new = 0,1
-    for i in range(0,n):
+    old,new = 1,1
+    new2 = 1
+    for i in range(0,n - 2):
         new2 = old + new
         old = new
         new = new2
