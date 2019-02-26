9666b0fc465ff84faee35c091d5b0b36b182152a
Paul Harden <paulharden@Pauls-iMac.hvc.rr.com>
2019-02-26 00:08:09 -0500

    created empty functions for order of operations, multi/div then add/sub, created user input string

--- project/calc_function.py
+++ project/calc_function.py
@@ -1,5 +1,13 @@
 # Calculations for Project
 
+def multDiv():
+   print("working so far..")
+   
+def addSub():
+   print("")
+   
 def main():
-
+    myList =input("Enter your equation (use spaces between operators): ")
+    multDiv()
+    
 main()
