f08b07f748834a60a355b816f5d55a87757edf44
Paul Harden <paulharden@Pauls-MacBook-Pro.local>
2019-02-28 17:46:19 -0500

    completed part 2

--- credentials.py
+++ credentials.py
@@ -5,9 +5,14 @@
 
 # Function to create a Marist-style username from user input
 # Generates username in only lowercase
-def makeName(first, last):
-    uname = first[0] + "." + last[:7]
-    return uname.lower()
+# Make list from returned user input?
+def getName():
+    first = input("Enter your first name: ")
+    last = input("Enter your last name: ")
+    return [first,last]
+
+def makeName(name):
+    return name[0] + "." + name[1]
 
 # Function to check strength of password
 # Checks to see if password length is >8 characters
@@ -17,18 +22,14 @@ def passCheck():
     while len(passwd)<8:  
         print("I sense a disturbance in the force. Your password should contain 8 or more characters.")
         passwd = input("Create a new password: ")
-    ##while len(passwd)<8:  
-        ##print("I sense a disturbance in the force. Your password could be better...")
-        ##passwd = input("Create a new password: ")
+    return passwd
     
 def main():
 
     # get user's first and last names
-    first = input("Enter your first name: ")
-    last = input("Enter your last name: ")
-
+    name = getName()
     # TODO modify this to generate a Marist-style username
-    uname = makeName(first, last)
+    uname = makeName(name)
     print("Your newly created username is:",f'"{uname}"')
 
     # ask user to create a new password
