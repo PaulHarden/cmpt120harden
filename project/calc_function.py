ee76d562f569ec7b28adcd778335072235d4df73
Paul Harden <paulharden@Pauls-MacBook-Pro.local>
2019-03-05 13:06:07 -0500

    created functions to receive input from user, convert input to list and check list for operator symbols. Created another function where the equations will be processed and the result will be returned in a new list.

--- project/calc_function.py
+++ project/calc_function.py
@@ -1,13 +1,26 @@
 # Calculations for Project
 
-def multDiv():
-   print("working so far..")
-   
-def addSub():
-   print("")
+def doMath(equation):
+   i = 0
+   while len(equation)>1 and hasProdDiv(equation, i):
+      if equation[i] == "*":
+         equation = process(equation, i)
+      if equation[i] == "/":
+         equation = process(equation, i)
+      if equation[i] == "+":
+         equation = process(equation, i)
+      if equation[i] == "-":
+         equation = process(equation, i)
+
+def process(equation, i):
+   for i in range(equation):
+      op1 = i - 1
+      op2 = i + 1
+      result = return [op1 * op2]
    
 def main():
-    myList =input("Enter your equation (use spaces between operators): ")
-    multDiv()
-    
+   equation = input("Enter a mathematical equation (with spaces between characters): ").split()
+   result = doMath(equation)
+   print("The result is",result)
+   
 main()
