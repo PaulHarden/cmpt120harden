f3f77a1e6ffd6e81ef2ddbd58ae1b6439d8e6d1a
Paul Harden <paulharden@Pauls-MacBook-Pro.local>
2019-03-07 17:45:38 -0500

    moved main function to new calculator file, imported calculations file. GUI will be built here.

--- project/calculator.py
+++ project/calculator.py
@@ -1,5 +1,10 @@
 # Calculator GUI
 
-def main():
+import cal_function
 
+def main():
+   equation = input("Enter a mathematical equation (with spaces between characters): ").split()
+   result = doMath(equation)
+   print("The result is",result)
+   
 main()
