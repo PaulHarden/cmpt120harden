20f0dfaab261c0a6dcf6d9412e644d5458801230
Paul Harden <paulharden@Pauls-MacBook-Pro.local>
2019-03-07 17:34:59 -0500

    rewrite: created two while loops to check for operator symbols, gave precedence to * & / with separate function, all arithmatic is done in a final function.

--- project/calc_function.py
+++ project/calc_function.py
@@ -2,25 +2,37 @@
 
 def doMath(equation):
    i = 0
-   while len(equation)>1 and hasProdDiv(equation, i):
-      if equation[i] == "*":
-         equation = process(equation, i)
-      if equation[i] == "/":
-         equation = process(equation, i)
-      if equation[i] == "+":
-         equation = process(equation, i)
-      if equation[i] == "-":
-         equation = process(equation, i)
+   while hasPrec(equation):
+      if equation[i] == "*" or equation[i] == "/":
+         process(equation, i)
+      else:
+         i = i + 1
+   i = 0   
+   while len(equation)>1:
+      if equation[i] == "+" or equation[i] == "-":
+         process(equation, i)
+      else:
+         i = i + 1
+         
+   return float(equation[0])
 
+def hasPrec(equation):
+   eqstr = "".join(equation)
+   if "*" in eqstr or "/" in eqstr:
+      return True
+   return False
+
+   
 def process(equation, i):
-   for i in range(equation):
-      op1 = i - 1
-      op2 = i + 1
-      result = return [op1 * op2]
-   
-def main():
-   equation = input("Enter a mathematical equation (with spaces between characters): ").split()
-   result = doMath(equation)
-   print("The result is",result)
-   
-main()
+   if equation[i] == "*":
+      result = float(equation[i - 1]) * float(equation[i + 1])
+   elif equation[i] == "/":
+      result = float(equation[i - 1]) / float(equation[i + 1])
+   elif equation[i] == "+":
+      result = float(equation[i - 1]) + float(equation[i + 1])
+   elif equation[i] == "-":
+      result = float(equation[i - 1]) - float(equation[i + 1])
+   del equation[i - 1]
+   del equation[i - 1]
+   del equation[i - 1]
+   equation.insert(i - 1, str(result))
