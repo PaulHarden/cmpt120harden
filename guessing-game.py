e7322bb680d037be69f7d004d588fb3b7a7d8dc7
Paul Harden <paulharden@Pauls-MBP.hvc.rr.com>
2019-03-29 22:46:54 -0400

    Game evaluates input from user in separate function and returns appropriate answer. Repeats with second question if non-accepted answer is given. Does not interfere with and breaks from first while loop. Resolves #5.

--- guessing-game.py
+++ guessing-game.py
@@ -5,10 +5,23 @@ def main():
         answer = input("Can you guess what it is?: ").lower()
         if answer == animal:
             print("Congratulations! You guessed correctly!")
+            nextAnswer = question()
             break
         elif answer[0] == "q":
             break
         else:
             print("Your guess was incorrect, try again!")
-  
+
+def question():
+    while True:
+        nextAnswer = input("Do you like snakes? (y or n): ").lower()
+        if nextAnswer == "y":
+            print("Me too! They certainly are interesting!")
+            break
+        elif nextAnswer == "n":
+            print("Me neither! They're creepy!")
+            break
+        else:
+            print("That's not what I asked...")
+    
 main()
