5ead2579f801098b1f369bd34836d26c6b536e15
Paul Harden <paulharden@Pauls-iMac.hvc.rr.com>
2019-04-18 14:47:04 -0400

    Wrote full, varied responses to be randomly selected for each emotion. Formatted all displayed text with line breaks and spacing. Removed all directions and commented to-dos.

--- personality.py
+++ personality.py
@@ -1,19 +1,17 @@
-# Get the mode of interaction from the user
-# Params: none
-# Returns: an integer indicating one of reward, punish, joke, or threaten
-
 import random
 
 def introduction():
-    print("Hey, I'm an AI!")
-    print("Right now I'm feeling...")
+    print("Hey, I'm a new AI and I'm feeling pretty good!")
+    print("")
 
 def ending():
-    print("Nice chatting...")
+    print("")
+    print("Nice chatting with you...")
     print("Until next time!")
+    print("")
 
 def primaryLoop():
-    currEmotion = 0
+    currEmotion = 3
     while True:
         userAction = getInteraction()
         if userAction == 4:
@@ -22,30 +20,51 @@ def primaryLoop():
         showEmotion(currEmotion, userAction)
 
 def getInteraction():
-    userAction = int(input("Specify an action(0-Reward, 1-Punish, 2-Threaten, 3-Joke, 4-Quit):"))# TODO prompt user to choose an action
-    return userAction # return a corresponding integer
-
-# Based on a given emotion and action, determine the next emotional state
-# Params:
-# currEmotion - a current emotion
-# userAction - a user interaction
-# Returns: an emotion
+    userAction = int(input("0: Reward\n1: Punish\n2: Threaten\n3: Joke\n4: Quit\nSpecify an Action (by number): "))
+    return userAction
+    
 
 def lookupEmotion(currEmotion, userAction):
-    emotionMatrix = [[3,0,2,5,3,3], # TODO do the matrix lookup
+    emotionMatrix = [[3,0,2,5,3,3],
                      [0,1,4,4,1,4],
                      [1,0,2,2,2,2],
                      [1,0,1,3,3,3]]
-    return emotionMatrix[userAction][currEmotion] # return an integer corresponding to an emotion
+    return emotionMatrix[userAction][currEmotion]
 
 def showEmotion(currEmotion, userAction):
-        response = [["Anger 1","Anger 2","Anger 3"],
-                    ["Disgust 1","Disgust 2","Disgust 3"],
-                    ["Fear 1","Fear 2","Fear 3"],
-                    ["Happiness 1","Happiness 2","Happiness 3"],
-                    ["Sadness 1","Sadness 2","Sadness 3"],
-                    ["Surprised 1", "Surprised 2","Surprised 3"]]
+        response = [["Angry... Get away from me!\n",
+                     "Angry... Why did you have to say that?\n",
+                     "Angry... What are you even doing here?\n",
+                     "Angry... What are you thinking!\n",
+                     "Angry... Just get out!\n"],
+                    ["Disgusted... How could you?!\n",
+                     "Disgusted... What the hell, man!\n",
+                     "Disgusted... Whatever...\n",
+                     "Disgusted... You're annoying.\n",
+                     "Disgusted... Ugh, go somewhere."],
+                    ["Fearful... Please don't!\n",
+                     "Fearful... But why??\n",
+                     "Fearful... I'm too young!\n",
+                     "Fearful... Stay away!\n",
+                     "Fearful... You're a monster!"],
+                    ["Happy... Feelin' good!\n",
+                     "Happy... That's great!\n",
+                     "Happy... Fantastic!\n",
+                     "Happy... Awesome!!\n",
+                     "Happy... Enjoying the internet!\n"],
+                    ["Sad... Wow, that cut deep...\n",
+                     "Sad... Please just leave me alone.\n",
+                     "Sad... Well, that's not very nice of you.\n",
+                     "Sad... Just unplug me.\n",
+                     "Sad... Why don't you go outside for a while?\n"],
+                    ["Surprised... Hooray!\n",
+                     "Surprised... What! No way!\n",
+                     "Surprised... Thank you so much!\n",
+                     "Surprised... You shouldn't have!\n",
+                     "Surprised... You're the best!\n"]]
+        print("")
         print(random.choice(response[currEmotion]))
+        print("")
     
 def main():
     introduction()
